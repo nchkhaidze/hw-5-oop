aa1ebbb251db3cb82388bf53ead0ff7d2b4564a8
nchkhaidze <58772264+nchkhaidze@users.noreply.github.com>
2020-11-17 15:45:53 +0300

    Fixed bug with salad prices

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 /* I made a simple GUI for extra convenience.
-    When the calculate price button is pressed the processOrder function is executed.
-    At the end of the script, testing is executed using all the methods of Order class. */
+    When the calculate price button is pressed the processOrder function is executed. */
 
 var form = document.querySelector('form');
 form.addEventListener('submit', processOrder);
-
-// Constants
-
-var itemsSet = {
-    smallHamburgerCheese: new Hamburger('small', new Stuffing('cheese', 10, 20)),
-    smallHamburgerSalad: new Hamburger('small', new Stuffing('salad', 20, 5)),
-    smallHamburgerPotato: new Hamburger('small', new Stuffing('potato', 15, 10)),
-
-    largeHamburgerCheese: new Hamburger('large', new Stuffing('cheese', 10, 20)),
-    largeHamburgerSalad: new Hamburger('large', new Stuffing('salad', 20, 5)),
-    largeHamburgerPotato: new Hamburger('large', new Stuffing('potato', 15, 10)),
-
-    cola: new Drink('cola', 50, 40),
-    coffee: new Drink('coffee', 80, 20),
-
-    cesarSalad: new Salad('cesar salad', 100, 20),
-    olivierSalad: new Salad('olivier salad', 50, 80),
-};
 
 function processOrder(event) {
     // Prevent the form from refreshing on submit
@@ -50,6 +31,9 @@ function processOrder(event) {
         }
     });
 
+    console.log(order);
+    order.showItems();
+
     // Outputting the result
 
     var outputField = document.querySelector('.output-field');
@@ -61,7 +45,6 @@ function processOrder(event) {
 }
 
 // Order Class
-
 function Order() {
     this.items = Array.prototype.slice.call(arguments);
     this.paid = false;
@@ -70,7 +53,7 @@ function Order() {
 Order.prototype.showItems = function () {
     console.log('Your order is:')
     for (var i = 0; i < this.items.length; i++) {
-        if (this.items[i] instanceof Salad) {
+        if (this.items[i].getType() === 'salad') {
             console.log(this.items[i].weight + ' grams of ' + this.items[i].name);
             break;
         }
@@ -83,13 +66,16 @@ Order.prototype.addItem = function (item) {
         return;
     }
     // If item is a salad, stack it by weight instead of adding one more
-    if (item instanceof Salad) {
+    if (item.getType() === 'salad') {
         var itemIndex = this.items.indexOf(item);
 
-        if (itemIndex !== -1) {
-            this.items[itemIndex].weight += 100;
-            return;
+        if (itemIndex === -1) {
+            item.setWeight(100)
+            this.items.push(item);
+        } else {
+            this.items[itemIndex].setWeight(this.items[itemIndex].weight + 100);
         }
+        return;
     }
     this.items.push(item);
 };
@@ -120,11 +106,11 @@ Order.prototype.pay = function () {
 };
 
 // Food Item - general class for food items
-
-function FoodItem(name, price, calories) {
+function FoodItem(name, price, calories, type) {
     this.name = name;
     this.price = price;
     this.calories = calories;
+    this.type = type;
 }
 
 FoodItem.prototype.getPrice = function () {
@@ -135,25 +121,28 @@ FoodItem.prototype.getCalories = function () {
     return this.calories;
 }
 
-// Drink class extends FoodItem
-
-function Drink(name, price, calories) {
-    FoodItem.apply(this, arguments);
+FoodItem.prototype.getType = function() {
+    return this.type;
 }
 
-Drink.__proto__ = FoodItem;
-Drink.prototype.__proto__ = FoodItem.prototype;
+// Drink class extends FoodItem
+function Drink(name, price, calories) {
+    FoodItem.apply(this, arguments);
+    this.type = 'drink';
+}
+
+Drink.prototype = Object.create(FoodItem.prototype);
+Drink.prototype.constructor = Drink;
 
 // Salad class extends FoodItem
-
 function Salad(name, price, calories) {
     FoodItem.apply(this, arguments);
+    this.type = 'salad';
     this.weight = 100;
 }
 
-// Make prototype assignment through __proto__ and not Object.create() so as not to break instanceof check
-Salad.__proto__ = FoodItem;
-Salad.prototype.__proto__ = FoodItem.prototype;
+Salad.prototype = Object.create(FoodItem.prototype);
+Salad.prototype.constructor = Salad;
 
 Salad.prototype.getPrice = function () {
     return (this.weight / 100 * this.price);
@@ -163,29 +152,32 @@ Salad.prototype.getCalories = function () {
     return (this.weight / 100 * this.calories);
 }
 
-// Stuffing class extends FoodItem
-
-function Stuffing(name, price, calories) {
-    FoodItem.apply(this, arguments);
+Salad.prototype.setWeight = function(weight) {
+    this.weight = weight;
 }
 
-Stuffing.__proto__ = FoodItem;
-Stuffing.prototype.__proto__ = FoodItem.prototype;
+// Stuffing class extends FoodItem
+function Stuffing(name, price, calories) {
+    FoodItem.apply(this, arguments);
+    this.type = 'stuffing';
+}
+
+Stuffing.prototype = Object.create(FoodItem.prototype);
+Stuffing.prototype.constructor = Stuffing;
 
 // Hamburger class extends FoodItem, accepts FoodItem object as stuffing argument
-
 function Hamburger(size, stuffing) {
     this.size = size;
     this.stuffing = stuffing;
     if (this.size == 'large') {
-        FoodItem.call(this, 'Large Hamburger with ' + this.stuffing.name, 100, 40);
+        FoodItem.call(this, 'Large Hamburger with ' + this.stuffing.name, 100, 40, 'hamburger');
     } else {
-        FoodItem.call(this, 'Small Hamburger with ' + this.stuffing.name, 50, 20)
+        FoodItem.call(this, 'Small Hamburger with ' + this.stuffing.name, 50, 20, 'hamburger');
     }
 }
 
-Hamburger.__proto__ = FoodItem;
-Hamburger.prototype.__proto__ = FoodItem.prototype;
+Hamburger.prototype = Object.create(FoodItem.prototype);
+Hamburger.prototype.constructor = Hamburger;
 
 Hamburger.prototype.getPrice = function () {
     return this.price + this.stuffing.price;
@@ -195,21 +187,20 @@ Hamburger.prototype.getCalories = function () {
     return this.calories + this.stuffing.calories;
 }
 
-// Testing
+// Constants
+var itemsSet = {
+    smallHamburgerCheese: new Hamburger('small', new Stuffing('cheese', 10, 20)),
+    smallHamburgerSalad: new Hamburger('small', new Stuffing('salad', 20, 5)),
+    smallHamburgerPotato: new Hamburger('small', new Stuffing('potato', 15, 10)),
 
-var order = new Order();
+    largeHamburgerCheese: new Hamburger('large', new Stuffing('cheese', 10, 20)),
+    largeHamburgerSalad: new Hamburger('large', new Stuffing('salad', 20, 5)),
+    largeHamburgerPotato: new Hamburger('large', new Stuffing('potato', 15, 10)),
 
-order.addItem(itemsSet.largeHamburgerSalad);
-order.addItem(itemsSet.smallHamburgerPotato);
-order.addItem(itemsSet.coffee);
-order.addItem(itemsSet.olivierSalad);
-order.addItem(itemsSet.olivierSalad);
-order.addItem(itemsSet.olivierSalad);
-order.pay();
-order.removeItem(itemsSet.coffee);
-order.removeItem(itemsSet.olivierSalad);
+    cola: new Drink('cola', 50, 40),
+    coffee: new Drink('coffee', 80, 20),
 
+    cesarSalad: new Salad('cesar salad', 100, 20),
+    olivierSalad: new Salad('olivier salad', 50, 80),
+};
 
-console.log(order.items);
-order.showItems();
-console.log(order.getPrice(), order.getCalories());
